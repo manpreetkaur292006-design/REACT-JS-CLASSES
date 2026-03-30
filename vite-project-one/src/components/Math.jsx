ef6459f5e5f47b7bf7263ea99d3b394d837b704a
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-03-30 15:51:47 +0530

    made some changes

--- vite-project-one/src/components/Math.jsx
+++ vite-project-one/src/components/Math.jsx
@@ -1,2 +1,6 @@
 export const add=(a,b)=>a+b;
 export const subtract=(a,b)=>a-b;
+// main.jsx  me strictmode ke wajah se do baar 
+// output ayega it is only for testing purpose
+// to see output one time we have to remove
+// or comment the strictmode
