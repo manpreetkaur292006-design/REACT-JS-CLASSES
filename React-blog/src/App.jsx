edcd1ae66e01824b120f406c2225c2d1d754d5e3
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-04-21 19:12:25 +0530

    added the app.jsx file

--- React-blog/src/App.jsx
+++ React-blog/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './component/Home'
+import Bollywood from './component/Bollywood'
+import Hollywood from './component/Hollywood'
+import Technology from './component/Technology'
+import Fitness from './component/Fitness'
+import Food from './component/Food'
+import NavBar from './component/NavBar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+
+        <NavBar/>
+
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/bollywood" element={<Bollywood/>}/>
+          <Route path="/hollywood" element={<Hollywood/>}/>
+          <Route path="/technology" element={<Technology/>}/>
+          <Route path="/fitness" element={<Fitness/>}/>
+          <Route path="/food" element={<Food/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
