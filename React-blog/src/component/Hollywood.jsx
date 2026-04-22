7384834fa8533781bbc504b36a1e8c31c902e932
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-04-22 19:23:21 +0530

    added the css for the different cards

--- React-blog/src/component/Hollywood.jsx
+++ React-blog/src/component/Hollywood.jsx
@@ -6,15 +6,15 @@ const Hollywood = () => {
   const HollywoodData = Data.filter((item)=>item.category==="Hollywood");
 
   return (
-    <>
+    <div className='blog-section-container'>
       {HollywoodData.map((hollydata)=>(
-        <>
+        <div className='blog-section-div'>
           <h1 className='blog-section-head'>{hollydata.title}</h1>
           <img src={hollydata.img_url} className='blog-section-img'/>
           <p className='blog-section-desc'>{hollydata.description}</p>
-        </>
+        </div>
       ))}
-    </>
+    </div>
   )
 }
 
