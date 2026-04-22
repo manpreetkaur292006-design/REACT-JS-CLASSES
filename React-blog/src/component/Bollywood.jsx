a4f4a1ac80dcb0728c918c8436d6b2fb4ba5cc59
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-04-22 19:22:22 +0530

    added the css for the different cards

--- React-blog/src/component/Bollywood.jsx
+++ React-blog/src/component/Bollywood.jsx
@@ -6,15 +6,15 @@ const Bollywood = () => {
   const BollywoodData = Data.filter((item)=>item.category==="Bollywood");
 
   return (
-    <>
+    <div className='blog-section-container'>
       {BollywoodData.map((bollydata)=>(
-        <>
+        <div className='blog-section-div'>
           <h1 className='blog-section-head'>{bollydata.title}</h1>
           <img src={bollydata.img_url} className='blog-section-img'/>
           <p className='blog-section-desc'>{bollydata.description}</p>
-        </>
+        </div>
       ))}
-    </>
+    </div>
   )
 }
 
