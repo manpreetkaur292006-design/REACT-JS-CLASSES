601eef31d01bf8d6ce88fea66eea3fe728f8b5b9
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-04-22 19:22:40 +0530

    added the css for the different cards

--- React-blog/src/component/Fitness.jsx
+++ React-blog/src/component/Fitness.jsx
@@ -6,15 +6,15 @@ const Fitness = () => {
   const FitnessData = Data.filter((item)=>item.category==="Fitness");
 
   return (
-    <>
+    <div className='blog-section-container'>
       {FitnessData.map((fitnessdata)=>(
-        <>
+        <div className='blog-section-div'>
           <h1 className='blog-section-head'>{fitnessdata.title}</h1>
           <img src={fitnessdata.img_url} className='blog-section-img'/>
           <p className='blog-section-desc'>{fitnessdata.description}</p>
-        </>
+        </div>
       ))}
-    </>
+    </div>
   )
 }
 
