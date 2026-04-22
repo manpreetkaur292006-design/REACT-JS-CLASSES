7d3ec032338802644bf65e6044c515941f733dfb
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-04-22 18:27:32 +0530

    added some code in the file

--- React-blog/src/component/Food.jsx
+++ React-blog/src/component/Food.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Data from '../Data'
 
 const Food = () => {
+
+  const FoodData = Data.filter((item)=>item.category==="Food");
+
   return (
-    <div>Food</div>
+    <>
+      {FoodData.map((fooddata)=>(
+        <>
+          <h1>{fooddata.title}</h1>
+          <img src={fooddata.img_url}/>
+          <p>{fooddata.description}</p>
+        </>
+      ))}
+    </>
   )
 }
 
