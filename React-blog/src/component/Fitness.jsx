73251b417e0baccbbe8f94e028f39641ee84e1f3
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-04-22 18:27:12 +0530

    made some changes

--- React-blog/src/component/Fitness.jsx
+++ React-blog/src/component/Fitness.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Data from '../Data'
 
 const Fitness = () => {
+
+  const FitnessData = Data.filter((item)=>item.category==="Fitness");
+
   return (
-    <div>Fitness</div>
+    <>
+      {FitnessData.map((fitnessdata)=>(
+        <>
+          <h1>{fitnessdata.title}</h1>
+          <img src={fitnessdata.img_url}/>
+          <p>{fitnessdata.description}</p>
+        </>
+      ))}
+    </>
   )
 }
 
