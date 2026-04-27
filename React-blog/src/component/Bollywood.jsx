68e5171f9ef28b6d6ee0315fc8bdc936a886c0b4
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-04-27 15:25:03 +0530

    added the links

--- React-blog/src/component/Bollywood.jsx
+++ React-blog/src/component/Bollywood.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import Data from '../Data'
+import React from "react";
+import Data from "../Data";
+import { Link } from "react-router-dom";
 
 const Bollywood = () => {
-
-  const BollywoodData = Data.filter((item)=>item.category==="Bollywood");
+  const BollywoodData = Data.filter((item) => item.category === "Bollywood");
 
   return (
     <>
-    <h1>Bollywood</h1>
-    <div className='blog-section-container'>
-      {BollywoodData.map((bollydata)=>(
-        <div className='blog-section-div'>
-          <h1 className='blog-section-head'>{bollydata.title}</h1>
-          <img src={bollydata.img_url} className='blog-section-img'/>
-          <p className='blog-section-desc'>{bollydata.description}</p>
-        </div>
-      ))}
-    </div>
+      <h1>Bollywood</h1>
+      <div className="blog-section-container">
+        {BollywoodData.map((bollydata) => (
+          <Link to={`/details/${bollydata.id}`}>
+            <div className="blog-section-div">
+              <h1 className="blog-section-head">{bollydata.title}</h1>
+              <img src={bollydata.img_url} className="blog-section-img" />
+              <p className="blog-section-desc">{bollydata.description}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Bollywood
+export default Bollywood;
