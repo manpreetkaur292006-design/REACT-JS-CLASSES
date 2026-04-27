0522b836107c98c1aee122409db1689de17222e0
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-04-27 15:24:53 +0530

    added the links

--- React-blog/src/component/Fitness.jsx
+++ React-blog/src/component/Fitness.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import Data from '../Data'
+import React from "react";
+import Data from "../Data";
+import { Link } from "react-router-dom";
 
 const Fitness = () => {
-
-  const FitnessData = Data.filter((item)=>item.category==="Fitness");
+  const FitnessData = Data.filter((item) => item.category === "Fitness");
 
   return (
     <>
-    <h1>Fitness</h1>
-    <div className='blog-section-container'>
-      {FitnessData.map((fitnessdata)=>(
-        <div className='blog-section-div'>
-          <h1 className='blog-section-head'>{fitnessdata.title}</h1>
-          <img src={fitnessdata.img_url} className='blog-section-img'/>
-          <p className='blog-section-desc'>{fitnessdata.description}</p>
-        </div>
-      ))}
-    </div>
+      <h1>Fitness</h1>
+      <div className="blog-section-container">
+        {FitnessData.map((fitnessdata) => (
+          <Link to={`/details/${fitnessdata.id}`}>
+            <div className="blog-section-div">
+              <h1 className="blog-section-head">{fitnessdata.title}</h1>
+              <img src={fitnessdata.img_url} className="blog-section-img" />
+              <p className="blog-section-desc">{fitnessdata.description}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Fitness
+export default Fitness;
