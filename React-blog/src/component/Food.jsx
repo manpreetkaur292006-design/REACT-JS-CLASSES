aff4d48434cbb46fe55c83a856dbd87aba3be838
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-04-27 15:24:39 +0530

    added the links

--- React-blog/src/component/Food.jsx
+++ React-blog/src/component/Food.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import Data from '../Data'
+import React from "react";
+import Data from "../Data";
+import { Link } from "react-router-dom";
 
 const Food = () => {
-
-  const FoodData = Data.filter((item)=>item.category==="Food");
+  const FoodData = Data.filter((item) => item.category === "Food");
 
   return (
     <>
-    <h1>Food</h1>
-    <div className='blog-section-container'>
-      {FoodData.map((fooddata)=>(
-        <div className='blog-section-div'>
-          <h1 className='blog-section-head'>{fooddata.title}</h1>
-          <img src={fooddata.img_url} className='blog-section-img'/>
-          <p className='blog-section-desc'>{fooddata.description}</p>
-        </div>
-      ))}
-    </div>
+      <h1>Food</h1>
+      <div className="blog-section-container">
+        {FoodData.map((fooddata) => (
+          <Link to={`/details/${fooddata.id}`}>
+            <div className="blog-section-div">
+              <h1 className="blog-section-head">{fooddata.title}</h1>
+              <img src={fooddata.img_url} className="blog-section-img" />
+              <p className="blog-section-desc">{fooddata.description}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Food
+export default Food;
