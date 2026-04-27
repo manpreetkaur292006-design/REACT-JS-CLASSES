8b4c66114267014d55a6b255a0199d6a83c79014
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-04-27 15:24:12 +0530

    added the links

--- React-blog/src/component/Technology.jsx
+++ React-blog/src/component/Technology.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import Data from '../Data'
+import React from "react";
+import Data from "../Data";
+import { Link } from "react-router-dom";
 
 const Technology = () => {
-
-  const TechnologyData=Data.filter((item)=>item.category==="Technology");
+  const TechnologyData = Data.filter((item) => item.category === "Technology");
 
   return (
     <>
-    <h1>Technology</h1>
-    <div className='blog-section-container'>
-      {TechnologyData.map((techdata)=>(
-        <div className='blog-section-div'>
-          <h1 className='blog-section-head'>{techdata.title}</h1>
-          <img src={techdata.img_url} className='blog-section-img'/>
-          <p className='blog-section-desc'>{techdata.description}</p>
-        </div>
-      ))}
-    </div>
+      <h1>Technology</h1>
+      <div className="blog-section-container">
+        {TechnologyData.map((techdata) => (
+          <Link to={`/details/${techdata.id}`}>
+            <div className="blog-section-div">
+              <h1 className="blog-section-head">{techdata.title}</h1>
+              <img src={techdata.img_url} className="blog-section-img" />
+              <p className="blog-section-desc">{techdata.description}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Technology
+export default Technology;
