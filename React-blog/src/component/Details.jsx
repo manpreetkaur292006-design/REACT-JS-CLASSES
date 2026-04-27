ff6b4013f9487e70e4939a49199ad18279c51fde
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-04-27 15:41:01 +0530

    added the usenavigate hook

--- React-blog/src/component/Details.jsx
+++ React-blog/src/component/Details.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Data from "../Data";
 
 const Details = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
+
   const post = Data.find((item) => item.id === parseInt(id));
   console.log(post, "deatils-post-line9");
 
@@ -12,6 +14,7 @@ const Details = () => {
   }
   return (
     <div>
+      <button onClick={()=>navigate(-1)}>Back</button>
       <h1>{post.title}</h1>
       <img src={post.img_url} />
       <p>{post.description}</p>
