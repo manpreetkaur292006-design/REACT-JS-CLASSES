183b8420c9c4df0f78562a366399ae977cc5fb95
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-04-29 14:15:45 +0530

    added the app.jsx file

--- React-blog/src/App.jsx
+++ React-blog/src/App.jsx
@@ -1,36 +1,38 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './component/Home'
-import Bollywood from './component/Bollywood'
-import Hollywood from './component/Hollywood'
-import Technology from './component/Technology'
-import Fitness from './component/Fitness'
-import Food from './component/Food'
-import NavBar from './component/NavBar'
-import Details from './component/Details'
+import React, { useContext } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./component/Home";
+import Bollywood from "./component/Bollywood";
+import Hollywood from "./component/Hollywood";
+import Technology from "./component/Technology";
+import Fitness from "./component/Fitness";
+import Food from "./component/Food";
+import NavBar from "./component/NavBar";
+import Details from "./component/Details";
+import { ThemeContext } from "./context/ThemeContext";
 
 const App = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <>
-      <BrowserRouter>
+      <div className={theme}>
+        <BrowserRouter>
+          <NavBar />
 
-        <NavBar/>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/bollywood" element={<Bollywood />} />
+            <Route path="/hollywood" element={<Hollywood />} />
+            <Route path="/technology" element={<Technology />} />
+            <Route path="/fitness" element={<Fitness />} />
+            <Route path="/food" element={<Food />} />
 
-        <Routes>
-          
-          <Route path="/" element={<Home/>}/>
-          <Route path="/bollywood" element={<Bollywood/>}/>
-          <Route path="/hollywood" element={<Hollywood/>}/>
-          <Route path="/technology" element={<Technology/>}/>
-          <Route path="/fitness" element={<Fitness/>}/>
-          <Route path="/food" element={<Food/>}/>
-
-          <Route path="/details/:id" element={<Details/>}/>
-
-        </Routes>
-      </BrowserRouter>
+            <Route path="/details/:id" element={<Details />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
